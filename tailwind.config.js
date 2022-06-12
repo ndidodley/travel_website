05fb2cdc02824314c5f0d164b700e90a61f1c921
ndidodley <88383651+ndidodley@users.noreply.github.com>
2022-06-12 04:00:43 +0100

    Added a huge section of pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}", ],
-  theme: {
-    extend: {},
-    fontFamily: {
-      'sans': ['Poppins','Inter'],
-    }
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}",],
+    theme: {
+        extend: {
+            colors: {
+                'text': {
+                    100: '',
+                    200: '#5E6282',
+                    300: '',
+                    400: '',
+                    500: '#14183E',
+                },
+                'accent': {
+                    100: '#F1A501',
+                    200: '',
+                    300: '#F1A501',
+                    400: '',
+                    500: '#DF6951',
+                }
+            }
+        },
+        fontFamily: {
+            'sans': ['Poppins', 'Inter'],
+        },
+    },
+    plugins: [],
 }
